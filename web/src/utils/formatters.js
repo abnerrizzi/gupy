ee6251b9cde675adbb8443792cec0796437c1107
Abner Rizzi <abner.rizzi@gmail.com>
2026-04-20 08:56:00 -0300

    Update job type formatters to group and translate all database types

--- web/src/utils/formatters.js
+++ web/src/utils/formatters.js
@@ -17,7 +17,7 @@ export const formatWorkplaceType = (type) => {
 };
 
 export const formatJobType = (type) => {
-  if (!type) return 'N/A';
+  if (!type || type === 'N/A') return 'Outros';
   switch (type.toLowerCase()) {
     case 'vacancy_type_effective':
     case 'efetivo':
@@ -26,9 +26,29 @@ export const formatJobType = (type) => {
     case 'vacancy_type_talent_pool':
     case 'banco de talentos':
       return 'Banco de Talentos';
+    case 'vacancy_type_internship':
     case 'estágio':
     case 'internship':
       return 'Estágio';
+    case 'vacancy_type_apprentice':
+    case 'aprendiz':
+      return 'Jovem Aprendiz';
+    case 'vacancy_type_temporary':
+    case 'temporário':
+      return 'Temporário';
+    case 'vacancy_type_lecturer':
+    case 'palestrante':
+    case 'docente':
+      return 'Docente';
+    case 'vacancy_legal_entity':
+    case 'pj':
+      return 'PJ';
+    case 'vacancy_type_associate':
+    case 'associado':
+      return 'Associado';
+    case 'vacancy_type_autonomous':
+    case 'autônomo':
+      return 'Autônomo';
     default:
       return type;
   }
